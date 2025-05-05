8201881f0a87fec11110685d7aeecec468e50836
James Krolik <53527367+JimKrolik@users.noreply.github.com>
2025-05-05 13:18:33 -0500

    Update Submit bypassing required fields.js

--- Submit bypassing required fields.js
+++ Submit bypassing required fields.js
@@ -6,7 +6,7 @@ Objective:  Submit a form when fields are still marked as required but unset.
 Use Case:   We have a form with one or multiple fields we do not want set on submission in order to speed processing up.
             In this case, a button labeled 'More Information Needed' and a radio button for approval as part of a collection of unknown size.
             To speed this up, we can just temporarily hide the field(s) and allow submission.
-            We use an async call with an await in order to ensure the field(s) hide before continuing to process.
+            We use an asynchronous function call with an await in order to ensure the field(s) hide before continuing to process.
 
 Button Label:    More Information Needed
 Target fieldID:  44 
